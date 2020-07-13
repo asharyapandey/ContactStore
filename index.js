8b260e806c70c33f7a027cca380140385022009a
asharyapandey <asharyap7@gmail.com>
2020-07-13 09:47:28 +0545

    added home page

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 // setting view engine to ejs
 app.set("view engine", "ejs");
 
+// setting public folder
+app.use(express.static("public"));
+
 app.use(express.json({ extended: false }));
 
 app.use("/", require("./Routes/contact"));
